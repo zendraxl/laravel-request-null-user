42680d69f320bd7327dde7abe65c43542fa22d63
Drazen Vasiljevic <zendraxl@gmail.com>
2019-07-18 18:01:32 +0200

    Add __call and __set to Owner

--- src/Owner.php
+++ src/Owner.php
@@ -15,9 +15,19 @@ class Owner
         $this->user = $user;
     }
 
+    public function __call($name, $arguments)
+    {
+        return $this->user->{$name}($arguments);
+    }
+
     public function __get($name)
     {
         return $this->user->{$name} ?? null;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->user->{$name} = $value;
     }
 
     public function isGuest(): bool
