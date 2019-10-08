635741431782a77b014b572c1eb6719266a6ad1e
Nemanja Maric <nemanjammaric@gmail.com>
2019-10-08 15:59:36 +0200

    Allow developers to pick theirs default values

--- src/NullUser.php
+++ src/NullUser.php
@@ -4,11 +4,6 @@ namespace Zendraxl\LaravelRequestNullUser;
 
 class NullUser
 {
-    protected const DEFAULT_EMAIL = 'guest@example.com';
-    protected const DEFAULT_NAME = 'Guest';
-    protected const DEFAULT_PROPERTY = 'type';
-    protected const DEFAULT_TYPE = 'guest';
-
     public $email;
     public $name;
     public $type;
@@ -20,22 +15,22 @@ class NullUser
 
     public static function defaultEmail(): string
     {
-        return env('ZENDRAXL_NULL_USER_EMAIL', static::DEFAULT_EMAIL);
+        return env('ZENDRAXL_NULL_USER_EMAIL', config('usernull.DEFAULT_EMAIL'));
     }
 
     public static function defaultName(): string
     {
-        return env('ZENDRAXL_NULL_USER_NAME', static::DEFAULT_NAME);
+        return env('ZENDRAXL_NULL_USER_NAME', config('usernull.DEFAULT_NAME'));
     }
 
     public static function defaultProperty(): string
     {
-        return env('ZENDRAXL_TYPE_PROPERTY', static::DEFAULT_PROPERTY);
+        return env('ZENDRAXL_TYPE_PROPERTY', config('usernull.DEFAULT_PROPERTY'));
     }
 
     public static function defaultType(): string
     {
-        return env('ZENDRAXL_NULL_USER_TYPE', static::DEFAULT_TYPE);
+        return env('ZENDRAXL_NULL_USER_TYPE', config('usernull.DEFAULT_TYPE'));
     }
 
     protected function setDefaults(): void
